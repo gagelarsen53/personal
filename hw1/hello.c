1b15c1d3ea1d6febe7ce6421706b5a7cc428d6e1
Gage Larsen <gagelarsen53@gmail.com>
2014-09-18 16:57:07 -0700

    Fixed the issues with hello.c this was just a problem with to many new lines.

--- hw1/hello.c
+++ hw1/hello.c
@@ -5,13 +5,13 @@ int main(void)
   int MAXLENGTH = 100;
   char name[MAXLENGTH];
 
-  printf("\nGage Larsen\n");
+  printf("Gage Larsen\n");
   printf("10645288\n\n");
 
   printf("What is your name? ");
   scanf("%s", name);
 
-  printf("Hello, %s\n\n", name);
+  printf("Hello %s\n", name);
 
   return 0;
 }
